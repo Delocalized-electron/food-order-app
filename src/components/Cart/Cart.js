e623f5360c37399685cba276f75cdc79b7b4b778
Aadil Khatri <aadil.khatri343@gmail.com>
2023-03-16 17:11:33 +0530

    Completed Close of cart component

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -18,14 +18,16 @@ const Cart = (props) => {
   );
 
   return (
-    <Modal>
+    <Modal onClose={props.onClose}>
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
         <span>35.42</span>
       </div>
       <div className={classes.actions}>
-        <button className={classes["button--alt"]}>close</button>
+        <button className={classes["button--alt"]} onClick={props.onClose}>
+          close
+        </button>
         <button className={classes.button}>order</button>
       </div>
     </Modal>
