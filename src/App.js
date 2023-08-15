a095aa6f191794125343a120198990f9c677cea6
Aadil Khatri <aadil.khatri343@gmail.com>
2023-08-15 10:30:35 +0530

    Added http request to get data from DB instead of hardcoded data

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Fragment, useState } from "react";
+import { useState } from "react";
 import Cart from "./components/Cart/Cart";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
